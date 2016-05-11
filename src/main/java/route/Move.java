26e44d6e81dc5f7ebd88425a0870a44bd1f40dd7
dbasak <dbasak@bol.com>
2016-05-12 00:48:08 +0200

    Working app
    Change-Id: I33de03741faed08093e71d64595babde6be4ba3e

---
+++ src/main/java/route/Move.java
@@ -0,0 +1,37 @@
+package route;
+
+import piece.Board;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import utils.Color;
+import utils.PositionException;
+import utils.PositionVector;
+
+public class Move implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+
+        try {
+
+            String from = request.queryParams("from");
+            String to = request.queryParams("to");
+
+            String[] fromSplit = from.split(",");
+            String[] toSplit = to.split(",");
+
+            String color = request.queryParams("color");
+            Board.move(color,
+                    new PositionVector(Integer.valueOf(fromSplit[0]), Integer.valueOf(fromSplit[1])),
+                    new PositionVector(Integer.valueOf(toSplit[0]), Integer.valueOf(toSplit[1])));
+
+            response.cookie("turn", Color.getOther(color));
+            response.redirect("/play");
+
+        } catch (PositionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/route/SetOption.java
@@ -0,0 +1,16 @@
+package route;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class SetOption implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        String color = request.queryParams("color");
+        response.cookie("color", color);
+        response.redirect("/play");
+        return null;
+    }
+}
